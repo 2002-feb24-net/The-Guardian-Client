bd7d1609303df869bd374e711cd20d1486f8493a
Angelica Velez <anvelez26@gmail.com>
2020-05-08 22:50:01 -0500

    k8s

--- GuardianClient/src/environments/environment.prod.ts
+++ GuardianClient/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  guardianApiBaseUrl: 'http://af73e5f4c8bd54a91ba71829cd98b089-1937973816.us-east-2.elb.amazonaws.com'
 };
--- GuardianClient/src/environments/environment.ts
+++ GuardianClient/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  guardianApiBaseUrl: 'http://af73e5f4c8bd54a91ba71829cd98b089-1937973816.us-east-2.elb.amazonaws.com'
 };
 
 /*
