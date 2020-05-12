dcada8ef0d36af205b1126cc16d0de1c267eb48d
Angelica Velez <anvelez26@gmail.com>
2020-05-11 19:49:07 -0500

    forgot the s so everything was pointless of course

--- GuardianClient/src/environments/environment.ts
+++ GuardianClient/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
  // guardianApiBaseUrl: 'http://af73e5f4c8bd54a91ba71829cd98b089-1937973816.us-east-2.elb.amazonaws.com'
-  guardianApiBaseUrl: 'http://guardian-api.2002.revaturelabs.com/'
+  guardianApiBaseUrl: 'https://guardian-api.2002.revaturelabs.com/'
 };
 
 /*
