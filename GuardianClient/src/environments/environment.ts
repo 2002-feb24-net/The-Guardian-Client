68a0ce1da5ff177514bbb84d100492f15b4dd4ab
Angelica Velez <anvelez26@gmail.com>
2020-05-11 19:45:11 -0500

    k8s https support

--- GuardianClient/src/environments/environment.ts
+++ GuardianClient/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  guardianApiBaseUrl: 'http://af73e5f4c8bd54a91ba71829cd98b089-1937973816.us-east-2.elb.amazonaws.com'
+ // guardianApiBaseUrl: 'http://af73e5f4c8bd54a91ba71829cd98b089-1937973816.us-east-2.elb.amazonaws.com'
+  guardianApiBaseUrl: 'http://guardian-api.2002.revaturelabs.com/'
 };
 
 /*
